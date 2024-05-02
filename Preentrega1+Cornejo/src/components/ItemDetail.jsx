ed28fcd7f6ba17442b783f77272ae49fa54678d9
Gormm <juan.cornejo.s@gmail.com>
2024-05-01 22:57:46 -0400

    changes itemdetails

--- Preentrega1+Cornejo/src/components/ItemDetail.jsx
+++ Preentrega1+Cornejo/src/components/ItemDetail.jsx
@@ -2,7 +2,6 @@ import ItemCount from "./ItemCount";
 
 const ItemDetail = ({item}) => {
     return (
-        <div className="card">
             <div className="row">
                 <div className="col-md-4 offset-md-2">
                     <img src={item.imagen} className="img-fluid" alt={item.nombre} />
@@ -14,7 +13,7 @@ const ItemDetail = ({item}) => {
                     <ItemCount stock={item.stock} />
                 </div>
             </div>
-        </div>
+
     )
 }
 
