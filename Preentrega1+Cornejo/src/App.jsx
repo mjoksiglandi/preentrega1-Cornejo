4f8aed624f42b709d4fbced7909cc5802508bd82
Gormm <juan.cornejo.s@gmail.com>
2024-05-28 15:37:39 -0400

    add, fire base, and configs

--- Preentrega1+Cornejo/src/App.jsx
+++ Preentrega1+Cornejo/src/App.jsx
@@ -5,20 +5,27 @@ import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import ItemDetailContainer from "./components/ItemDetailContainer"
 import Error404 from "./components/Error404"
+import Checkout from "./components/Checkout";
+import CartContextProvider from "./components/context/CartContext";
+import Cart from "./components/Cart";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path={"/"} element={<ItemListContainer />} />
-        <Route path={"/category/:id"} element={<ItemListContainer />} />
-        <Route path={"/item/:id"} element={<ItemDetailContainer />} />
-        <Route path={"*"} element={<Error404 />} />
-      </Routes>
-      <Banner />
-      <Footer />
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<ItemListContainer />} />
+          <Route path={"/category/:id"} element={<ItemListContainer />} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"/cart"} element={<Cart />} />
+          <Route path={"/checkout"} element={<Checkout />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        <Banner />
+        <Footer />
+      </BrowserRouter>
+    </CartContextProvider>
   )
 }
 
--- Preentrega1+Cornejo/src/main.jsx
+++ Preentrega1+Cornejo/src/main.jsx
@@ -3,4 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAaYszeyqjqXSpwlovU9RrvOKCbd1wFWoc",
+    authDomain: "trashpandagarage-4abd6.firebaseapp.com",
+    databaseURL: "https://trashpandagarage-4abd6-default-rtdb.firebaseio.com",
+    projectId: "trashpandagarage-4abd6",
+    storageBucket: "trashpandagarage-4abd6.appspot.com",
+    messagingSenderId: "1010689914185",
+    appId: "1:1010689914185:web:ccaea182f9c8aff4179698"
+};
+
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
