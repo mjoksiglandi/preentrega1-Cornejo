cce4e0a00a4ef31a0be0b5478bc0b78ac5532034
Gormm <juan.cornejo.s@gmail.com>
2024-05-30 17:18:08 -0400

    fire base re config

--- Preentrega1+Cornejo/src/components/ItemDetail.jsx
+++ Preentrega1+Cornejo/src/components/ItemDetail.jsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
 import ItemCount from "./ItemCount";
+import { CartContext } from "./context/CartContext";
+
 
 const ItemDetail = ({item}) => {
+    const {addItem} = useContext(CartContext);
+
+    const onAdd = (quantity) => {
+        addItem(item, quantity);
+        console.log("Agregaste: " + quantity + " productos!");
+    }
+
     return (
+        <div className="container">
             <div className="row">
                 <div className="col-md-4 offset-md-2">
                     <img src={item.imagen} className="img-fluid" alt={item.nombre} />
@@ -10,10 +21,10 @@ const ItemDetail = ({item}) => {
                     <h1 className="fs-3 text-uppercase fw-semibold">{item.nombre}</h1>
                     <p className="fs-4 fw-bold">${item.precio}</p>
                     <p className="small">{item.descripcion}</p>
-                    <ItemCount stock={item.stock} />
+                    <ItemCount stock={item.stock} onAdd={onAdd} />
                 </div>
             </div>
-
+        </div>
     )
 }
 
